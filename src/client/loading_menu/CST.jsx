9295c4524454ee0b721210c247210e641fe6d250
kingdapster <sal.carrazco89@gmail.com>
2024-01-18 13:17:05 -0800

    merge conflicts resolved

--- src/client/loading_menu/CST.jsx
+++ src/client/loading_menu/CST.jsx
@@ -3,6 +3,11 @@ export const CST = {
        
         LOAD: "LOAD",
         MENU: "MENU",
+<<<<<<< Updated upstream
         LEVEL1: "LEVEL1"
+=======
+        LEVEL1: "LEVEL1",
+        PAUSE: "PAUSE"
+>>>>>>> Stashed changes
     }
 }
