49ee8f162ef37a35a2d3baa07bac437293b2088b
kingdapster <sal.carrazco89@gmail.com>
2024-01-26 08:05:51 -0800

    old branch push

--- src/client/PhaserGame.jsx
+++ src/client/PhaserGame.jsx
@@ -1,5 +1,5 @@
-
 import Phaser from 'phaser';
+import WebFont from 'webfontloader';
 import {Level1} from './level1';
 import {Load} from "./loading_menu/LoadingScene.jsx"
 import {Menu} from './loading_menu/MenuScene';
@@ -7,7 +7,6 @@ import {PauseScene} from './pauseScene';
 
 import {LoginScene} from './loading_menu/Login_Phaser.jsx';
 import { CST } from './loading_menu/CST.jsx';
-
 
 const PhaserGame = () => {
   var config = {
@@ -18,7 +17,7 @@ const PhaserGame = () => {
     // line required fr use of dom elements
     dom: {
       createContainer: true
-  },
+    },
     physics: {
         default: 'arcade',
         arcade: {
@@ -26,14 +25,20 @@ const PhaserGame = () => {
         }
     },
     scene:[ LoginScene, Load, Menu, Level1, PauseScene]
-  
+  };
 
-    };
-    
-    var game = new Phaser.Game(config);
-
-    return <div id="phaser-game"></div>;
-    };
-    
+  WebFont.load({
+    custom: {
+        families: ['p-script', 'pixle script bold']
+    },
+    active: function() {
+        console.log('font loaded');
+        // Once the fonts are loaded, start the Phaser game
+        var game = new Phaser.Game(config);
+    }
+  });
+//old code
+  return <div id="phaser-game"></div>;
+};
 
 export default PhaserGame;
